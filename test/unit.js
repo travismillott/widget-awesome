2bf5380d7b9620c6350f964fec7e3ca768aabbb6
Travis Millott <travis.millott@gmail.com>
2017-02-16 12:06:26 -0700

    revert unittest

--- test/unit.js
+++ test/unit.js
@@ -1,9 +1,6 @@
-import React from 'react'
 var test = require('tape')
 
-import { likeButton } from '../jsx/Page.jsx'
-
-test('Button can only be hit once', function (test) {
-  test.equal(likeButton, "somediv", "button hit")
-  test.end()
+test('fill in this', function (t) {
+  t.ok(true)
+  t.end()
 })
